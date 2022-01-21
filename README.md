40c6f31d0ea216ab4064e960ce06d85853e96b3f
Lane <lanebirm2@gmail.com>
2022-01-22 08:57:58 +1000

    Inital commit with question files and inital script setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 9B_coding_question
+
+Repo for NineByte coding question. Provided question and test files found in to root of this repository.
+tile-elit.pdf is the coding question.
+
---
+++ tile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Created By: Lane Birmingham
+# Created Date: 22/01/2022
+
+def bytes32_to_tile():
+    '''reads stdin of a 32 byte file, converts to 10x10 tile, then outputs TILE to stdout'''
+
+if __name__ == '__main__':
+    bytes32_to_tile()
